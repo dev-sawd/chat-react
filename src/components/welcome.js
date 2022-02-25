526f65dcc9b0b277470989a217a8a9dbf4643225
devsawd <devsawd@gmail.com>
2022-02-25 15:31:41 +0900

    weclome compnent 작성

---
+++ src/components/welcome.js
@@ -0,0 +1,23 @@
+import React from "react"
+import Typography from "@mui/material/Typography";
+
+const Welcome = (props) => {
+    return (
+        <div style={props.style}>
+            <div
+                style={{
+                    position: 'absolute', left: '50%', top: '50%',
+                    transform: 'translate(-50%, -50%)'
+                }}
+            >
+                <Typography style={{marginLeft: 100, fontWeight: "bold", color: 'white'}}
+                            variant="h4" component="div" gutterBottom>
+                    user님 환영합니다<br/><br/>
+                    왼쪽 로그인 유저 리스트를 선택해 새 대화를 시작하세요.
+                </Typography>
+            </div>
+        </div>
+    )
+}
+
+export default Welcome
