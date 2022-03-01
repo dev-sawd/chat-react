9afedebd6a98cea3758c552a4b622555341b7dd7
요태 <devsawd@gmail.com>
2022-03-01 19:40:15 +0900

    infoMessageBox.js - 부모의 중앙에 위치하도록 정렬 수정

--- src/components/infoMessageBox.js
+++ src/components/infoMessageBox.js
@@ -4,15 +4,15 @@ const InfoMessageBox = (props) => {
     return (
         <div style={{
             display: "flex",
-            flexDirection: "row",
-            alignSelf: "center"
+            flexDirection: "column",
         }}>
 
             <div style={{
                 borderRadius: 20,
                 backgroundColor: "#b4ce76",
                 padding: 10,
-                margin: 10
+                margin: 10,
+                alignSelf: 'center'
             }}>
                 {props.targetUserName}님과 채팅이 시작되었습니다
             </div>
